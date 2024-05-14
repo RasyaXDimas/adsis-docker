4697c89271befbd4c4d9f76139d50041f00c4093
Finn <bagusalfin50@gmail.com>
2024-05-14 13:14:45 +0700

    Ubah template persona

--- src/Component/Persona.jsx
+++ src/Component/Persona.jsx
@@ -5,41 +5,82 @@ const Persona = ({
 	role,
 	foto,
 	quote,
-	valueA,
-	valueEx,
-	valueJb,
-	valueSk,
+	goals,
+	skill,
+	ability,
+	hobi,
+	invers,
 }) => {
 	return (
-		<div className="flex bg-[#C490E4] w-3/4 mx-auto rounded-xl mt-12 ring-2 ring-[#C490E4]">
-			<img className="align-middle" src={foto} alt="" />
-			<div className="grid grid-cols-4 p-8 w-full bg-[#F9F9F9] rounded-xl">
-				<div className="col-span-8">
+		<div
+			className={`flex bg-[#C490E4] w-3/4 mx-auto rounded-xl m-12 ring-4 ring-[#C490E4] ${
+				invers
+					? "drop-shadow-[-10px_10px_10px_rgba(196,144,228,1)] flex-row-reverse"
+					: "drop-shadow-[10px_10px_10px_rgba(196,144,228,1)]"
+			}`}
+		>
+			<img
+				className={`align-middle w-[25rem] h-[37rem]  ${
+					invers ? "rounded-e-xl" : "rounded-s-xl"
+				}`}
+				src={foto}
+				alt={`${nama}'s portrait`}
+			/>
+			<div
+				className={`grid grid-cols-2 gap-8 p-8 w-full bg-[#F9F9F9]  ${
+					invers ? "rounded-s-xl" : "rounded-e-xl"
+				}`}
+			>
+				<div className="col-span-2">
 					<h1 className="font-bold text-4xl">{nama}</h1>
 					<p className="font-light text-2xl">{role}</p>
 				</div>
-				<div className="col-span-8">
-					<p className="text-3xl font-bold mb-2">Motivation Quote</p>
+				<div className="col-span-2">
+					<p className="text-3xl font-bold mb-2">Moto Hidup</p>
 					<blockquote className="text-xl italic">{quote}</blockquote>
 				</div>
-				<div className="col-span-4 ">
+				<div className="col-span-1">
 					<div className="flex flex-col mt-4 mb-4">
-						<p className="text-3xl font-bold">Experience</p>
-						<p className="mt-2">{valueEx}</p>
+						<p className="text-3xl font-bold">Skill</p>
+						<ul className="mt-2">
+							{skill.map((value, index) => (
+								<li key={index} className="mt-1">
+									{value}
+								</li>
+							))}
+						</ul>
 					</div>
 					<div className="flex flex-col mt-4">
-						<p className="text-3xl font-bold">Skills</p>
-						<p className="mt-2">{valueSk}</p>
+						<p className="text-3xl font-bold">Hobi</p>
+						<ul className="mt-2">
+							{hobi.map((value, index) => (
+								<li key={index} className="mt-1">
+									{value}
+								</li>
+							))}
+						</ul>
 					</div>
 				</div>
-				<div className="col-span-4">
+				<div className="col-span-1">
 					<div className="flex flex-col mt-4">
-						<p className="text-3xl font-bold">Job</p>
-						<p className="mt-2">{valueJb}</p>
+						<p className="text-3xl font-bold">Ability</p>
+						<ul className="mt-2">
+							{ability.map((value, index) => (
+								<li key={index} className="mt-1">
+									{value}
+								</li>
+							))}
+						</ul>
 					</div>
 					<div className="flex flex-col mt-4">
-						<p className="text-3xl font-bold">Alasan Hidup</p>
-						<p className="mt-2">{valueA}</p>
+						<p className="text-3xl font-bold">Goal</p>
+						<ul className="mt-2">
+							{goals.map((value, index) => (
+								<li key={index} className="mt-1">
+									{value}
+								</li>
+							))}
+						</ul>
 					</div>
 				</div>
 			</div>
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import Persona from '../Component/Persona'
 import foto from "../assets/persona.png"
+import ref from "../assets/foto.jpg";
 
 const LandingPage = () => {
-  return (
-    <>
-        <Persona foto={foto} nama="Lenatheaa" role="Front End" quote="Jika terlalu menjaga image, hidupmu hanya sebatas jpeg" valueEx="First Blood 1000x"
-        valueSk="Html, Css, JUngler, Offlaner" valueJb="Gatau mau di isi apa" valueA="Untuk Bernafas" />
-    </>
-  )
-}
+	return (
+		<>
+			<Persona
+				foto={ref}
+				nama="Lenatheaa"
+				role="Front End"
+				quote="Jika terlalu menjaga image, hidupmu hanya sebatas jpeg"
+				ability={["Cantik", "Baik", "Pintar"]}
+				goals={["Jadi Docker"]}
+				invers={true}
+				hobi={["Membaca buku", "Mendengarkan musik", "Belajar"]}
+				skill={["Bedah jantung", "Operasi sesar", "Bisa pegang retri"]}
+			/>
+		</>
+	);
+};
 
 export default LandingPage
